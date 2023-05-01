858a8fd816e9e045df95b14ee543ff1e9271f36e
Daniel <danielkispert+github@googlemail.com>
2023-05-01 23:42:13 +0200

    broken state

--- src/main/java/com/daniel/jsoneditor/view/impl/jfx/impl/scenes/impl/editor/components/editorwindow/components/listview/ObjectFieldLayout.java
+++ src/main/java/com/daniel/jsoneditor/view/impl/jfx/impl/scenes/impl/editor/components/editorwindow/components/listview/ObjectFieldLayout.java
@@ -2,6 +2,7 @@ package com.daniel.jsoneditor.view.impl.jfx.impl.scenes.impl.editor.components.e
 
 import com.daniel.jsoneditor.view.impl.jfx.impl.scenes.impl.editor.components.editorwindow.EditorWindowManager;
 import com.daniel.jsoneditor.view.impl.jfx.impl.scenes.impl.editor.components.editorwindow.components.listview.field.EditorTextFieldFactory;
+import com.daniel.jsoneditor.view.impl.jfx.impl.scenes.impl.editor.components.editorwindow.components.listview.impl.EditorListViewImpl;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import javafx.geometry.Pos;
@@ -23,7 +24,7 @@ public class ObjectFieldLayout extends HBox
     {
         this.editorWindowManager = editorWindowManager;
         setAlignment(Pos.CENTER_LEFT);
-        VBox keyField = JsonEditorListView.makeFieldWithTitle("Key", node.getDisplayName());
+        VBox keyField = EditorListViewImpl.makeFieldWithTitle("Key", node.getDisplayName());
         getChildren().add(keyField);
         JsonNode value = node.getNode();
         if (value.isArray() || value.isObject())
