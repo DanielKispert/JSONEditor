5c904552faa0b37269033743ac225096084936a3
Daniel <danielkispert+github@googlemail.com>
2023-06-01 01:42:33 +0200

    smol changes

--- src/main/java/com/daniel/jsoneditor/view/impl/jfx/impl/scenes/impl/editor/components/editorwindow/components/listview/EditorTableView.java
+++ src/main/java/com/daniel/jsoneditor/view/impl/jfx/impl/scenes/impl/editor/components/editorwindow/components/listview/EditorTableView.java
@@ -6,5 +6,9 @@ import javafx.scene.control.TableView;
 
 public abstract class EditorTableView extends TableView<JsonNodeWithPath>
 {
+    /**
+     * selects a parent element. The table view will display a list of children (or items), depending on if the node is an object or an array
+     * @param selection
+     */
     public abstract void setSelection(JsonNodeWithPath selection);
 }
