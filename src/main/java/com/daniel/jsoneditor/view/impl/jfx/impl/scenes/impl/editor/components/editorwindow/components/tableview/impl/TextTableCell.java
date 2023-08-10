d1d285315a0b893c9c8fb933ec0d0f640fce2d08
Daniel Kispert <34270661+DanielKispert@users.noreply.github.com>
2023-08-10 16:58:26 +0200

    fixi

--- src/main/java/com/daniel/jsoneditor/view/impl/jfx/impl/scenes/impl/editor/components/editorwindow/components/tableview/impl/TextTableCell.java
+++ src/main/java/com/daniel/jsoneditor/view/impl/jfx/impl/scenes/impl/editor/components/editorwindow/components/tableview/impl/TextTableCell.java
@@ -3,56 +3,25 @@ package com.daniel.jsoneditor.view.impl.jfx.impl.scenes.impl.editor.components.e
 import com.daniel.jsoneditor.model.ReadableModel;
 import com.daniel.jsoneditor.model.json.JsonNodeWithPath;
 import com.daniel.jsoneditor.view.impl.jfx.impl.scenes.impl.editor.components.editorwindow.EditorWindowManager;
+import com.daniel.jsoneditor.view.impl.jfx.impl.scenes.impl.editor.components.editorwindow.components.tableview.field.AutofillingComboBox;
 import com.fasterxml.jackson.databind.node.ObjectNode;
-import javafx.scene.control.TextField;
+import javafx.scene.control.ComboBox;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+
 public class TextTableCell extends EditorTableCell
 {
-    
     private final ReadableModel model;
-    
-    private final TextField textField;
     
     public TextTableCell(EditorWindowManager manager, ReadableModel model)
     {
         super(manager);
         this.model = model;
-        textField = new TextField();
-    
-        {
-            textField.setOnAction(event ->
-            {
-                commitEdit(textField.getText());
-            });
-            textField.focusedProperty().addListener((obs, wasFocused, isNowFocused) ->
-            {
-                if (wasFocused && !isNowFocused)
-                {
-                    commitEdit(textField.getText());
-                }
-            });
-        }
     }
     
-    protected void updateAutofill()
-    {
-        if (getTableRow() == null || getTableRow().getItem() == null)
-        {
-            return;
-        }
-        JsonNodeWithPath parentNode = getTableRow().getItem();
-        EditorTableColumn column = ((EditorTableColumn) getTableColumn());
-        String propertyName = column.getPropertyName();
-        String path = parentNode.getPath() + "/" + propertyName;
-        final List<String> suggestions = model.getStringExamplesForPath(path);
-        final List<String> allowedValues = model.getAllowedStringValuesForPath(path);
-        
-        
-    }
+
     
     @Override
     protected void saveValue(JsonNodeWithPath item, String propertyName, String newValue)
@@ -73,9 +42,33 @@ public class TextTableCell extends EditorTableCell
         else
         {
             setText(null);
-            textField.setText(item);
-            setGraphic(textField);
-            updateAutofill();
+            setGraphic(makeComboBox(item));
         }
     }
+    
+    private AutofillingComboBox makeComboBox(String initialValue)
+    {
+        JsonNodeWithPath parentNode = getTableRow().getItem();
+        EditorTableColumn column = ((EditorTableColumn) getTableColumn());
+        String propertyName = column.getPropertyName();
+        String path = parentNode.getPath() + "/" + propertyName;
+        final List<String> examples = model.getStringExamplesForPath(path);
+        final List<String> allowedValues = model.getAllowedStringValuesForPath(path);
+        // if false, the user can enter values that are not in the suggestions. If true, they can't and must pick a value from the suggestions
+        boolean userEnteredValuesAllowed;
+        List<String> suggestions;
+        if (allowedValues.isEmpty())
+        {
+            suggestions = examples;
+            userEnteredValuesAllowed = true;
+        }
+        else
+        {
+            suggestions = allowedValues;
+            // there are required values, so the user must pick one of them
+            userEnteredValuesAllowed = false;
+        }
+        return new AutofillingComboBox(userEnteredValuesAllowed, initialValue, suggestions);
+    }
+    
 }
