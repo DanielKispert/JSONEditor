9c491b9a2ac44298dc45c1ddf312b66e6936ea60
Daniel Kispert <34270661+DanielKispert@users.noreply.github.com>
2023-08-12 18:27:51 +0200

    more improvements

--- src/main/java/com/daniel/jsoneditor/view/impl/jfx/impl/scenes/impl/editor/components/editorwindow/components/tableview/impl/fields/AutofillField.java
+++ src/main/java/com/daniel/jsoneditor/view/impl/jfx/impl/scenes/impl/editor/components/editorwindow/components/tableview/impl/fields/AutofillField.java
@@ -1,5 +1,44 @@
 package com.daniel.jsoneditor.view.impl.jfx.impl.scenes.impl.editor.components.editorwindow.components.tableview.impl.fields;
 
-public class AutofillField
+import com.daniel.jsoneditor.view.impl.jfx.impl.scenes.impl.editor.components.editorwindow.components.tableview.impl.TextTableCell;
+import javafx.collections.ObservableList;
+import javafx.scene.control.ComboBox;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class AutofillField extends ComboBox<String>
 {
+    
+    public AutofillField(TextTableCell parent, String text, List<String> allSuggestions, boolean valueMustBeASuggestion)
+    {
+        setEditable(!valueMustBeASuggestion);
+        getItems().setAll(allSuggestions);
+        setValue(text);
+        
+        setOnAction(event ->
+        {
+            parent.commitEdit(getEditor().getText());
+        });
+        
+        focusedProperty().addListener((obs, wasFocused, isNowFocused) ->
+        {
+            if (wasFocused && !isNowFocused)
+            {
+                parent.commitEdit(getEditor().getText());
+            }
+        });
+        
+        // Auto-complete behavior
+        getEditor().textProperty().addListener((observable, oldValue, newValue) ->
+        {
+            getItems().setAll(allSuggestions.stream()
+                                      .filter(suggestion -> suggestion.contains(newValue))
+                                      .collect(Collectors.toList()));
+            if (!isShowing())
+            {
+                show();
+            }
+        });
+    }
 }
