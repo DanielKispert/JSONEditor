bb45674190887f394bb766403e26c73d844f6b44
Daniel Kispert <34270661+DanielKispert@users.noreply.github.com>
2023-08-12 22:14:53 +0200

    maybe-fixed fields

--- src/main/java/com/daniel/jsoneditor/view/impl/jfx/impl/scenes/impl/editor/components/editorwindow/components/tableview/impl/fields/AutofillField.java
+++ src/main/java/com/daniel/jsoneditor/view/impl/jfx/impl/scenes/impl/editor/components/editorwindow/components/tableview/impl/fields/AutofillField.java
@@ -1,11 +1,9 @@
 package com.daniel.jsoneditor.view.impl.jfx.impl.scenes.impl.editor.components.editorwindow.components.tableview.impl.fields;
 
 import com.daniel.jsoneditor.view.impl.jfx.impl.scenes.impl.editor.components.editorwindow.components.tableview.impl.TextTableCell;
-import javafx.collections.ObservableList;
 import javafx.scene.control.ComboBox;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class AutofillField extends ComboBox<String>
 {
@@ -13,14 +11,10 @@ public class AutofillField extends ComboBox<String>
     public AutofillField(TextTableCell parent, String text, List<String> allSuggestions, boolean valueMustBeASuggestion)
     {
         setEditable(!valueMustBeASuggestion);
-        getItems().setAll(allSuggestions);
         setValue(text);
-        
-        setOnAction(event ->
-        {
-            parent.commitEdit(getEditor().getText());
-        });
-        
+    
+        setOnAction(event -> parent.commitEdit(getEditor().getText()));
+    
         focusedProperty().addListener((obs, wasFocused, isNowFocused) ->
         {
             if (wasFocused && !isNowFocused)
@@ -28,17 +22,7 @@ public class AutofillField extends ComboBox<String>
                 parent.commitEdit(getEditor().getText());
             }
         });
-        
-        // Auto-complete behavior
-        getEditor().textProperty().addListener((observable, oldValue, newValue) ->
-        {
-            getItems().setAll(allSuggestions.stream()
-                                      .filter(suggestion -> suggestion.contains(newValue))
-                                      .collect(Collectors.toList()));
-            if (!isShowing())
-            {
-                show();
-            }
-        });
+    
+        getItems().setAll(allSuggestions);
     }
 }
