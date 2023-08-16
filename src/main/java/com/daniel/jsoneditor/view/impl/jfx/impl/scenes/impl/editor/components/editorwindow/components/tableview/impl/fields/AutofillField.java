282769cae9a728c20e0c3c2eff434c28ad08434e
Daniel Kispert <34270661+DanielKispert@users.noreply.github.com>
2023-08-16 02:23:35 +0200

    added autofill

--- src/main/java/com/daniel/jsoneditor/view/impl/jfx/impl/scenes/impl/editor/components/editorwindow/components/tableview/impl/fields/AutofillField.java
+++ src/main/java/com/daniel/jsoneditor/view/impl/jfx/impl/scenes/impl/editor/components/editorwindow/components/tableview/impl/fields/AutofillField.java
@@ -2,19 +2,28 @@ package com.daniel.jsoneditor.view.impl.jfx.impl.scenes.impl.editor.components.e
 
 import com.daniel.jsoneditor.view.impl.jfx.impl.scenes.impl.editor.components.editorwindow.components.tableview.impl.TextTableCell;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.util.Callback;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class AutofillField extends ComboBox<String>
 {
     
+    
+    private final List<String> allSuggestions;
+    
     public AutofillField(TextTableCell parent, String text, List<String> allSuggestions, boolean valueMustBeASuggestion)
     {
+        this.allSuggestions = allSuggestions;
+        
         setEditable(!valueMustBeASuggestion);
         setValue(text);
-    
+        
         setOnAction(event -> parent.commitEdit(getEditor().getText()));
-    
+        
         focusedProperty().addListener((obs, wasFocused, isNowFocused) ->
         {
             if (wasFocused && !isNowFocused)
@@ -24,5 +33,33 @@ public class AutofillField extends ComboBox<String>
         });
     
         getItems().setAll(allSuggestions);
+    
+        if (!valueMustBeASuggestion)
+        {
+            getEditor().textProperty().addListener((observable, oldValue, newValue) ->
+                                                           filterSuggestions(newValue));
+        }
+    }
+    
+    private void filterSuggestions(String filterText)
+    {
+        hide();
+        List<String> filteredSuggestions = allSuggestions.stream()
+                                                   .filter(suggestion -> suggestion.toLowerCase().contains(filterText.toLowerCase()))
+                                                   .collect(Collectors.toList());
+        getSelectionModel().clearSelection();
+        setVisibleRowCount(filteredSuggestions.size());
+        getItems().setAll(filteredSuggestions);
+        if (!filteredSuggestions.isEmpty())
+        {
+            if (isFocused() && !isShowing())
+            {
+                show();
+            }
+        }
+        else
+        {
+            hide();
+        }
     }
 }
