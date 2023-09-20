6b2ce76cde564f30e34164d283166852277c83a8
Daniel Kispert <daniel.kispert@sap.com>
2023-09-20 15:52:11 +0200

    fixed int and double handling

--- src/main/java/com/daniel/jsoneditor/view/impl/jfx/impl/scenes/impl/editor/components/editorwindow/components/tableview/impl/NumberTableCell.java
+++ src/main/java/com/daniel/jsoneditor/view/impl/jfx/impl/scenes/impl/editor/components/editorwindow/components/tableview/impl/NumberTableCell.java
@@ -1,5 +1,7 @@
 package com.daniel.jsoneditor.view.impl.jfx.impl.scenes.impl.editor.components.editorwindow.components.tableview.impl;
 
+import java.math.BigDecimal;
+
 import com.daniel.jsoneditor.model.json.JsonNodeWithPath;
 import com.daniel.jsoneditor.view.impl.jfx.impl.scenes.impl.editor.components.editorwindow.EditorWindowManager;
 import com.fasterxml.jackson.databind.node.ObjectNode;
@@ -14,6 +16,22 @@ public class NumberTableCell extends EditorTableCell
     @Override
     protected void saveValue(JsonNodeWithPath item, String propertyName, String newValue)
     {
-        ((ObjectNode) item.getNode()).put(propertyName, Double.parseDouble(newValue));
+        try
+        {
+            int valueAsInt = Integer.parseInt(newValue);
+            ((ObjectNode) item.getNode()).put(propertyName, valueAsInt);
+        }
+        catch (NumberFormatException e)
+        {
+            try
+            {
+                double valueAsDouble = Double.parseDouble(newValue);
+                ((ObjectNode) item.getNode()).put(propertyName, valueAsDouble);
+            }
+            catch (NumberFormatException f)
+            {
+                // value is neither int nor double but in a number field, very problematic
+            }
+        }
     }
 }
