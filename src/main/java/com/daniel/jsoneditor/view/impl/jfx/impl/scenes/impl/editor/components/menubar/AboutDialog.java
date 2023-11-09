befc78c97060ea07f13ff9da65572c6d0facb562
Daniel Kispert <daniel.kispert@sap.com>
2023-11-09 11:42:14 +0100

    menu bar with about screen

---
+++ src/main/java/com/daniel/jsoneditor/view/impl/jfx/impl/scenes/impl/editor/components/menubar/AboutDialog.java
@@ -0,0 +1,30 @@
+package com.daniel.jsoneditor.view.impl.jfx.impl.scenes.impl.editor.components.menubar;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import javafx.scene.control.Alert;
+
+
+public class AboutDialog extends Alert
+{
+    public AboutDialog()
+    {
+        super(AlertType.INFORMATION);
+        Properties versionProperties = new Properties();
+        InputStream versionFile = getClass().getClassLoader().getResourceAsStream("version.properties");
+        try
+        {
+            versionProperties.load(versionFile);
+        }
+        catch (IOException e)
+        {
+            System.out.println("IOException on trying to load version property");
+        }
+        String versionNumber = versionProperties.getProperty("version");
+        setTitle("About");
+        setHeaderText("JSON Editor");
+        setContentText("Version " + versionNumber);
+    }
+}
