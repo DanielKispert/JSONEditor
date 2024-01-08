d443784de50d540815ddce2d6c0d6ad4a334463f
Daniel Kispert <34270661+DanielKispert@users.noreply.github.com>
2024-01-08 15:46:12 +0100

    fixed some refactoring bugs

---
+++ src/main/java/com/daniel/jsoneditor/model/json/schema/reference/HasKeyAndFancyName.java
@@ -0,0 +1,11 @@
+package com.daniel.jsoneditor.model.json.schema.reference;
+
+public interface HasKeyAndFancyName
+{
+    String getKey();
+    
+    String getPath();
+    
+    String getFancyName();
+    
+}
