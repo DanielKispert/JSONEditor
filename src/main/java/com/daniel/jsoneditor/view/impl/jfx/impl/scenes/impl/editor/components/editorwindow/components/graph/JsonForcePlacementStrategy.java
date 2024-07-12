f02f19b67238ec7fd5b7bf696e7cb31239efbdad
Daniel Kispert <34270661+DanielKispert@users.noreply.github.com>
2024-07-12 10:14:52 +0200

    strategy finetuning

--- src/main/java/com/daniel/jsoneditor/view/impl/jfx/impl/scenes/impl/editor/components/editorwindow/components/graph/JsonForcePlacementStrategy.java
+++ src/main/java/com/daniel/jsoneditor/view/impl/jfx/impl/scenes/impl/editor/components/editorwindow/components/graph/JsonForcePlacementStrategy.java
@@ -10,6 +10,6 @@ public class JsonForcePlacementStrategy extends ForceDirectedSpringGravityLayout
 {
     public JsonForcePlacementStrategy()
     {
-        super(25, 1, 10, 0.8, 0.01);
+        super(50, 1.5, 1, 1, 0.05);
     }
 }
