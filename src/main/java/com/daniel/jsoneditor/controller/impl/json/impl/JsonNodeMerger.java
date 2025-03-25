057e555ebddb62174479643d5382118a4678634b
Daniel Kispert <34270661+DanielKispert@users.noreply.github.com>
2025-03-25 01:52:27 +0100

    have to fix equality with referenceable object

--- src/main/java/com/daniel/jsoneditor/controller/impl/json/impl/JsonNodeMerger.java
+++ src/main/java/com/daniel/jsoneditor/controller/impl/json/impl/JsonNodeMerger.java
@@ -1,12 +1,14 @@
 package com.daniel.jsoneditor.controller.impl.json.impl;
 
+import com.daniel.jsoneditor.model.ReadableModel;
+import com.daniel.jsoneditor.model.json.JsonNodeWithPath;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
 public class JsonNodeMerger
 {
-    public static JsonNode createMergedNode(JsonNode baseNode, JsonNode updateNode)
+    public static JsonNode createMergedNode(ReadableModel model, JsonNodeWithPath baseNode, JsonNode updateNode)
     {
         if (baseNode == null)
         {
@@ -14,30 +16,66 @@ public class JsonNodeMerger
         }
         if (updateNode == null)
         {
-            return baseNode;
+            return baseNode.getNode();
         }
         if (baseNode.isArray() && updateNode.isArray())
         {
-            ArrayNode baseArray = (ArrayNode) baseNode;
+            ArrayNode baseArray = (ArrayNode) baseNode.getNode();
             ArrayNode updateArray = (ArrayNode) updateNode;
-            baseArray.addAll(updateArray); // TODO we can maybe replace this with equality checks later
-        
+            
+            // we want the base array to contain all elements of it, merge all elements that were in the update array, too
+            // and add all elements of the update array that werent in the base array
+            for (JsonNode updateElement : updateArray)
+            {
+                // if the update element is not an object or array we'll just add it to the base array
+                if (!updateElement.isObject() && !updateElement.isArray())
+                {
+                    baseArray.add(updateElement);
+                    continue;
+                }
+                // if the update element is an object or array we'll try to merge it into the base array if it's already in there
+                // we do merging via the ReferenceableObject framework
+                boolean containedInBaseArray = false;
+                JsonNode baseElementToMerge = null;
+                int foundIndex;
+                for (foundIndex = 0; foundIndex < baseArray.size(); foundIndex++)
+                {
+                    baseElementToMerge = baseArray.get(foundIndex);
+                    model.getReferenceableObject(baseNode.getPath() + "/" + foundIndex);
+                    if (baseElementToMerge.equals(updateElement)) //TODO fix equality
+                    {
+                        containedInBaseArray = true;
+                        break;
+                    }
+                }
+                if (containedInBaseArray)
+                {
+                    JsonNode mergedElement = createMergedNode(model,
+                            new JsonNodeWithPath(baseElementToMerge, baseNode.getPath() + "/" + foundIndex), updateElement);
+                    baseArray.set(foundIndex, mergedElement);
+                }
+                else
+                {
+                    baseArray.add(updateElement);
+                }
+            }
+            
             return baseArray;
         }
         else if (baseNode.isObject() && updateNode.isObject())
         {
-            ObjectNode baseObject = (ObjectNode) baseNode;
+            ObjectNode baseObject = (ObjectNode) baseNode.getNode();
             ObjectNode updateObject = (ObjectNode) updateNode;
         
             // Recursively merge object properties
-            updateObject.fields().forEachRemaining(entry ->
-            {
+            updateObject.fields().forEachRemaining(entry -> {
                 String key = entry.getKey();
                 JsonNode value = entry.getValue();
                 if (baseObject.has(key))
                 {
                     // If the key exists in baseObject, merge the values recursively
-                    JsonNode mergedValue = createMergedNode(baseObject.get(key), value);
+                    JsonNode mergedValue = createMergedNode(model,
+                            new JsonNodeWithPath(baseObject.get(key), baseNode.getPath() + "/" + key), value);
                     baseObject.set(key, mergedValue);
                 }
                 else
@@ -55,4 +93,7 @@ public class JsonNodeMerger
             return updateNode;
         }
     }
+    
+    
+    
 }
