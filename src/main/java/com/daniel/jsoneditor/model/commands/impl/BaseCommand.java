93f6b8cf296cc59f0fd35615fe4f0d308ec12dc6
Daniel Kispert <34270661+DanielKispert@users.noreply.github.com>
2025-09-28 15:25:56 +0200

    fixed merging errors

--- src/main/java/com/daniel/jsoneditor/model/commands/impl/BaseCommand.java
+++ src/main/java/com/daniel/jsoneditor/model/commands/impl/BaseCommand.java
@@ -6,21 +6,12 @@ import com.daniel.jsoneditor.model.changes.ModelChange;
 import java.util.Collections;
 import java.util.List;
 
-<<<<<<< Updated upstream
 public abstract class BaseCommand implements Command
 {
     protected final WritableModelInternal model;
     
     public BaseCommand(WritableModelInternal model)
     {
-=======
-public abstract class BaseCommand implements Command {
-    protected final ReadableModel readModel;
-    protected final WritableModelInternal model;
-
-    public BaseCommand(ReadableModel readModel, WritableModelInternal model) {
-        this.readModel = readModel;
->>>>>>> Stashed changes
         this.model = model;
     }
 
