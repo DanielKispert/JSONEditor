514846bb141ea07349a077eb14b13f4ed23da243
Daniel <danielkispert@gmx.de>
2025-09-28 15:25:56 +0200

    fixed merging errors

--- src/main/java/com/daniel/jsoneditor/model/commands/impl/AddNodeToArrayCommand.java
+++ src/main/java/com/daniel/jsoneditor/model/commands/impl/AddNodeToArrayCommand.java
@@ -9,7 +9,6 @@ import com.fasterxml.jackson.databind.JsonNode;
 
 import java.util.List;
 
-<<<<<<< Updated upstream
 public class AddNodeToArrayCommand extends BaseCommand
 {
     
@@ -21,40 +20,18 @@ public class AddNodeToArrayCommand extends BaseCommand
     {
         super(model);
         this.pathToArray = pathToArray;
-=======
-public class AddNodeToArrayCommand extends BaseCommand {
-    private final String path; // array path
-
-    public AddNodeToArrayCommand(ReadableModel readModel, WritableModelInternal model, String path) {
-        super(readModel, model);
-        this.path = path;
->>>>>>> Stashed changes
-    }
-
-    @Override
-<<<<<<< Updated upstream
-    public void execute()
-    {
-        int addedIndex = model.addNodeToArray(pathToArray);
-        pathOfAddedNode = pathToArray + "/" + addedIndex;
     }
     
-    @Override
-    public void undo()
-    {
-        model.removeNode(pathOfAddedNode);
-=======
+
     public List<ModelChange> execute() {
-        // create new node from schema
-        JsonNode newNode = readModel.makeArrayNode(path);
-        int index = model.addNodeToArray(path, newNode);
-        if (index == -1) {
+        
+        int addedIndex = model.addNodeToArray(pathToArray);
+        if (addedIndex == -1) {
             return noChanges();
         }
-        String newItemPath = path + "/" + index;
+        pathOfAddedNode = pathToArray + "/" + addedIndex;
         // keep legacy event behavior so existing UI still updates
-        model.sendEvent(new Event(EventEnum.ADDED_ITEM_TO_ARRAY_FROM_ARRAY, newItemPath));
-        return List.of(ModelChange.add(newItemPath, newNode));
->>>>>>> Stashed changes
+        model.sendEvent(new Event(EventEnum.ADDED_ITEM_TO_ARRAY_FROM_ARRAY, pathOfAddedNode));
+        return List.of(ModelChange.add(pathOfAddedNode, newNode));
     }
 }
