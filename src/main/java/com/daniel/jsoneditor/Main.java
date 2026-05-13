b1e9b31cba849633743da5e83023dcb066e54552
Daniel Kispert <34270661+DanielKispert@users.noreply.github.com>
2026-05-13 14:08:41 +0200

    fixed MacOS window controls

--- src/main/java/com/daniel/jsoneditor/Main.java
+++ src/main/java/com/daniel/jsoneditor/Main.java
@@ -1,11 +1,14 @@
 package com.daniel.jsoneditor;
 
+import java.awt.Toolkit;
+
 import com.daniel.jsoneditor.view.JFXLauncher;
 
 public class Main
 {
     public static void main(String[] args)
     {
+        Toolkit.getDefaultToolkit();
         JFXLauncher.launchJFXApplication(args);
     }
 }
